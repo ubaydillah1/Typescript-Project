482bbabb3b72a9e258b3fd820b5e6db9a8416e3e
ubaydillah1 <ubaydillah1737@gmail.com>
2024-12-03 00:13:39 +0700

    Create CustomMaps.ts and reusable class

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,18 +1,26 @@
 /// <reference types="@types/google.maps" />
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const person = new User();
-const company = new Company();
+// const person = new User();
+// const company = new Company();
 
-console.log(person);
-console.log(company);
-console.log(google);
+// console.log(person);
+// console.log(company);
+// console.log(google);
 
-const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+// const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+//   zoom: 1,
+//   center: {
+//     lat: 0,
+//     lng: 0,
+//   },
+// });
+
+import { CustomMap } from "./CustomMap";
+
+window.onload = () => {
+  new CustomMap("map");
+};
+
+console.log("LKdsjfl");
