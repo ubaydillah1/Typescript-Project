c77b441005274905ac705327a8e97d74a5cf279c
ubaydillah1 <ubaydillah1737@gmail.com>
2024-12-18 16:01:05 +0700

    add new branch for alternative stats

--- stats/build/CsvFileReader.js
+++ stats/build/CsvFileReader.js
@@ -18,8 +18,7 @@ class CsvFileReader {
             .split("\n")
             .map((row) => {
             return row.split(",");
-        })
-            .map(this.mapRow);
+        });
     }
 }
 exports.CsvFileReader = CsvFileReader;
