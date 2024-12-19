80bd13197d6e135699dcebc4e37391ef9990ef3a
ubaydillah1 <ubaydillah1737@gmail.com>
2024-12-19 19:16:18 +0700

    create analyzers and composition class

---
+++ stats/build/summary.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+class Summary {
+    constructor(analyzer, outputTarger) {
+        this.analyzer = analyzer;
+        this.outputTarger = outputTarger;
+    }
+}
+exports.Summary = Summary;
